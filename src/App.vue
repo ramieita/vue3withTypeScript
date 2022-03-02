11191be408aa89111034a74f1647f165969e4c51
Eita,Rami <s76042@beuth-hochschule.de>
2022-03-02 18:49:20 +0100

    add job type

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <br>
       <span> this is from ref to show how to use the generics with ref
 
-      {{ageWithRef}}
+      {{ ageWithRef }}
       </span>
     </p>
   </div>
@@ -15,22 +15,22 @@
 
 <script lang="ts">
 import {defineComponent, reactive, ref, toRefs} from 'vue';
-
+import Job from "@/types/job";
 
 export default defineComponent({
   name: 'App',
   components: {},
   setup() {
-    const state = reactive({
-      name: 'link',
-      age: 23 as string | number
 
-    })
+    const jobs = ref<Job[]>([
+      {title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1'},
+      {title: 'quarryman', location: 'death mountain', salary: 40000, id: '2'},
+      {title: 'flute player', location: 'the lost woods', salary: 35000, id: '3'},
+      {title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4'},
+      {title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'}
+    ])
+    return {jobs}
 
-    //  this is the same like above but here we use the ref from vue
-
-    const ageWithRef = ref<string | number>(24)
-    return {...toRefs(state),ageWithRef}
   }
 
 });
