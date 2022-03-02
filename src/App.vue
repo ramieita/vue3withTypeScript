2490d22bec9e17ecc689143f358a8b9cbd1d926d
Eita,Rami <s76042@beuth-hochschule.de>
2022-03-02 18:39:17 +0100

    ref with generics and  reactive

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-<div>
-  sd
-</div>
+  <div>
+    <p> {{ name }}
+      <br>
+      {{ age }}
+
+      <br>
+      <span> this is from ref to show how to use the generics with ref
+
+      {{ageWithRef}}
+      </span>
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, reactive, ref, toRefs} from 'vue';
 
 
 export default defineComponent({
   name: 'App',
-  components: {
+  components: {},
+  setup() {
+    const state = reactive({
+      name: 'link',
+      age: 23 as string | number
 
+    })
+
+    //  this is the same like above but here we use the ref from vue
+
+    const ageWithRef = ref<string | number>(24)
+    return {...toRefs(state),ageWithRef}
   }
+
 });
 </script>
 
